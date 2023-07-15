54165fc8a7c42a3ab6376f78f4ba229ededdf5b6
serruht <latemantis@gmail.com>
2023-07-15 22:47:18 +0800

    Remove old files

--- home/dot_config/nvim/lua/settings/outline.lua
+++
@@ -1,8 +0,0 @@
-vim.g.symbols_outline = {
-  show_guides = false,
-  show_symbol_details = false,
-  auto_preview = false,
-  highlight_hovered_item = false,
-  position = 'right',
-  width = 15,
-}
